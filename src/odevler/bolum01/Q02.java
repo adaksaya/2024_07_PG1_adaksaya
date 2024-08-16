61f973db637fa9179c91e215875d07ccd82207d2
adaksaya <23soft1022@isik.edu.tr>
2024-08-16 10:08:26 +0300

    Display five messages

--- src/odevler/bolum01/Q02.java
+++ src/odevler/bolum01/Q02.java
@@ -1,4 +1,12 @@
 package odevler.bolum01;
 
 public class Q02 {
+    //(Display five messages) Write a program that displays Welcome to Java five times.
+    public static void main(String[] args) {
+            System.out.println(" Welcome to Java");
+            System.out.println(" Welcome to Java");
+            System.out.println(" Welcome to Java");
+            System.out.println(" Welcome to Java");
+            System.out.println(" Welcome to Java");
+    }
 }
