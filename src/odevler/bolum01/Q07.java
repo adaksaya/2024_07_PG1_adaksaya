08ad2f5860e960af4f984f731916402994b539fa
adaksaya <23soft1022@isik.edu.tr>
2024-08-16 14:00:33 +0300

    Approximate

--- src/odevler/bolum01/Q07.java
+++ src/odevler/bolum01/Q07.java
@@ -4,9 +4,11 @@ public class Q07 {
     /*
     1.7 (Approximate) p can be computed using the following formula:
         π = 4 × ( 1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + ... )
+
      Write a program that displays the result of
         4 × ( 1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 )
         4 × ( 1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + 1/13 )
+
       Use 1.0 instead of 1 in your program.
      */
     public static void main(String[] args) {
