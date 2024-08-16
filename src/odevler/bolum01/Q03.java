2ab714e0b71f04e414e6022509be418c7c6d32c2
adaksaya <23soft1022@isik.edu.tr>
2024-08-16 10:21:39 +0300

    Display five messages

--- src/odevler/bolum01/Q03.java
+++ src/odevler/bolum01/Q03.java
@@ -3,9 +3,9 @@ package odevler.bolum01;
 public class Q03 {
     //1.3 (Display a pattern) Write a program that displays the following pattern:
     public static void main(String[] args) {
-        System.out.println("       J       A       V     V       A    ");
-        System.out.println("       J      A A       V   V       A A   ");
-        System.out.println("   J   J     A A A       V V       A A A  ");
-        System.out.println("    JJJ     A     A       V       A     A ");
+        System.out.println("       J       A       V     V       A     ");
+        System.out.println("       J      A A       V   V       A A    ");
+        System.out.println("   J   J     A A A       V V       A A A   ");
+        System.out.println("    JJJ     A     A       V       A     A  ");
     }
 }
